0164ff24fec5cc78957e3ce992b56d5ce22c0c60
Svein Manum <34545502+sveinbmaBDO@users.noreply.github.com>
2018-08-28 19:08:44 +0200

    Revert "merget"
    This reverts commit b7abd988c91e618d6b5a2f3c558163b271f4cb5f, reversing
    changes made to 2fd21596f3344796d358b91a76e75c2d2b99c361.

--- exercise-2/calc.js
+++ exercise-2/calc.js
@@ -23,7 +23,6 @@ document.addEventListener('DOMContentLoaded', function() {
   var updateCalculation = function() {
     var result = calculate(number1Field, number2Field, operationField);
 
-
     if (!Number.isNaN(result)) {
       resultField.innerText = result;
     }
